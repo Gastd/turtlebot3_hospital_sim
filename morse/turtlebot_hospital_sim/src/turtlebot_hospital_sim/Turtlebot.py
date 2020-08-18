54e6fb4cc647e9ce1e6611f7e5dd5a35bb35aa35
Gabriel Araujo <g.araujo@automni.com.br>
2020-08-18 08:56:27 -0300

    Add lidar for each robot

--- morse/turtlebot_hospital_sim/src/turtlebot_hospital_sim/Turtlebot.py
+++ morse/turtlebot_hospital_sim/src/turtlebot_hospital_sim/Turtlebot.py
@@ -12,10 +12,20 @@ class Turtlebot(Robot):
 
     def add_to_simulation(self, x=-19, y=-3, z=0,
                           x_rot=0, y_rot=0, z_rot=0):
-        self.translate(x, y, z)
         self.rotate(x_rot, y_rot, z_rot)
         self.add_motion_sensor()
         self.add_pose_sensor()
+        self.add_lidar_sensor()
+
+    def add_lidar_sensor(self):
+        self.lidar = Hokuyo()
+        self.append(self.lidar)
+        self.lidar.properties(Visible_arc = False)
+        self.lidar.properties(laser_range = 30.0)
+        self.lidar.properties(resolution = 1.0)
+        self.lidar.properties(scan_window = 180.0)
+        self.lidar.create_laser_arc()
+        self.lidar.add_interface('ros', topic=f"{self.name}/lidar")
 
     def add_motion_sensor(self):
         self.motion = MotionVW()
